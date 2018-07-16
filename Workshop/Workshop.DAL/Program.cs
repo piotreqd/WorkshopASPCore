1d0343556a8e811a45548fd5b690bf2d48587a3d
piotreqd <piotrduda919@gmail.com>
2018-07-16 19:47:02 +0200

    UselessCommentsDeleted

--- Workshop/Workshop.DAL/Program.cs
+++ Workshop/Workshop.DAL/Program.cs
@@ -9,31 +9,7 @@ namespace Workshop.DAL
     {
         static void Main(string[] args)
         {
-            /*var context = new WorkshopContext();
-            var owner = new Owner()
-            {
-                Name = "Kakashi",
-                Surname = "Hatake",
-                PhoneNumber = "222333444",
-                Cars = new List<Car>()
-                {
-                    new Car(){Mark = "renalut", Model = "megane", Registration = "swd333"},
-                    new Car(){Mark = "citroen", Model = "c2", Registration = "src333"},
-                }
-            };
-
-            var car = owner.Cars.First();
-            car.Orders = new List<Order>();
-            car.Orders.Add(
-                new Order()
-                {
-                    Name = "wymiana oleju",
-                    StartDate = DateTime.Now
-                }
-            );
-            context.Add(owner);
-            context.SaveChanges();
-            Console.WriteLine("dodano");*/
+            
         }
     }
 }
